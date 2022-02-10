6d72db9bbf187bfc76ae68b4d97bc9cc420ded96
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2022-02-09 20:42:36 -0500

    Applied `slock-quickcancel-1.4.diff`.

--- config.def.h
+++ config.def.h
@@ -10,3 +10,6 @@ static const char *colorname[NUMCOLS] = {
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* time in seconds to cancel lock with mouse movement */
+static const int timetocancel = 4;
